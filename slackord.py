d243492727ed1027058fde3dc7d2d415b04a30fb
Thomas Loupe <thomas.loupe@impelos.com>
2019-04-14 14:19:14 -0500

    4/14/19
    PEP8 Compliancy

--- slackord.py
+++ slackord.py
@@ -8,35 +8,47 @@ from discord.ext import commands
 # Insert the Discord bot token into the quoted section below
 TOKEN = ''
 
-bot = commands.Bot(command_prefix = '!')
+bot = commands.Bot(command_prefix='!')
 
 # Change the JSON file name in the quoted field below.
 with open('test.json') as json_file:
     data = json.load(json_file)
     for message in data:
-        # Print the messages we'll output to Discord from the Slack JSON file into console for the user to see.
-        if "real_name" in message.keys() and "datetime" in message.keys() and "text" in message.keys():
-            print((message["datetime"]) + (': ') + (message["real_name"]) + (': ') + (message["text"]))
+        # Print the messages we'll output to Discord
+        # from the Slack JSON file into console for the user to see.
+        if "real_name" in message.keys() and "datetime" in message.keys(
+        ) and "text" in message.keys():
+            print((message["datetime"]) + (': ') + (message["real_name"]) + (
+                ': ') + (message["text"]))
             print (' ')
         elif "bot_message" in message.keys() and "text" in message.keys():
-            print ("%s BOT MESSAGE" % (message["bot_message"]) + (': ') + (message["text"]))
+            print ("%s BOT MESSAGE" % (message["bot_message"]) + (
+                ': ') + (message["text"]))
+
 
 @bot.event
 async def on_ready():
-    print('Slackord is now ready to send messages to Discord! Type !mergeslack in the channel you wish to send messages to.')
+    print('Slackord is now ready to send messages to Discord!')
+    print('Type !mergeslack in the channel you wish to send messages to.')
     print(' ')
 
-# When !mergeslack is typed in a channel, iterate through the JSON file and post the message
+# When !mergeslack is typed in a channel, iterate
+# through the JSON file and post the message.
+
+
 @bot.command(pass_context=True)
 async def mergeslack(ctx):
     with open('test.json') as json_file:
         data = json.load(json_file)
         for message in data:
-            if "real_name" in message.keys() and "datetime" in message.keys() and "text" in message.keys():
-                messageToSend = ((message["datetime"]) + (': ') + (message["real_name"]) + (': ') + (message["text"]))
+            if "real_name" in message.keys() and "datetime" in message.keys(
+            ) and "text" in message.keys():
+                messageToSend = ((message["datetime"]) + (': ') + (
+                    message["real_name"]) + (': ') + (message["text"]))
                 await ctx.send(messageToSend)
             elif "bot_message" in message.keys() and "text" in message.keys():
-                messageToSend = ("%s BOT MESSAGE" % (message["bot_message"]) + (': ') + (message["text"]))
+                messageToSend = ("%s BOT MESSAGE" % (
+                    message["bot_message"]) + (': ') + (message["text"]))
                 await ctx.send(messageToSend)
 
 bot.run(TOKEN)
